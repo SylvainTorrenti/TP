76a74953eae745e59100732e3101f883dc23c31c
SylvainTorrenti <torrsyl@gmail.com>
2023-01-11 10:01:17 +0100

    Encapsulation
    -Mise en privé des attributs
    -Création des Get & Set
    -Modification créations des objet (Car & Person)
    -Ajout des methodes (Add & Remove Car/Owner)
    -Lecture des propriétées de Car & Person

--- TPVoiture/Car.cs
+++ TPVoiture/Car.cs
@@ -11,42 +11,148 @@ namespace TPVoiture
     /// </summary>
     internal class Car
     {
-        #region Attribute
-
+        #region Attributes
         /// <summary>
         /// Registartion of the car
         /// </summary>
-        public string Registration;
+        private string Registration;
         /// <summary>
         /// Model of the car
         /// </summary>
-        public string Model;
+        private string Model;
         /// <summary>
         /// Brand of the car
         /// </summary>
-        public string Brand;
+        private string Brand;
         /// <summary>
         /// How many klm the car has made
         /// </summary>
-        public int KLM;
+        private int KLM;
         /// <summary>
         /// First date when the car has been release
         /// </summary>
-        public DateOnly OriginalInServiceDate;
+        private DateOnly OriginalInServiceDate;
         /// <summary>
         /// Power of the car in horsepower
         /// </summary>
-        public int Power;
+        private int Power;
         /// <summary>
         /// Personal owner of the car
         /// </summary>
-        public Person Owner;
+        private Person? Owner; // "?" signifie que cet attribut peut être null
+        #endregion
+        #region Get & Set Registration
+        /// <summary>
+        /// Get Registration
+        /// </summary>
+        /// <returns>Registration</returns>
+        public string GetRegistration()
+        {
+            return Registration;
+        }
+        /// <summary>
+        /// Set registration
+        /// </summary>
+        /// <param name="Registration">Registration</param>
+        public void SetRegistration(string Registration)
+        {
+            this.Registration = Registration;
+        }
+        #endregion
+        #region Get & Set Model
+        /// <summary>
+        /// Get Model
+        /// </summary>
+        /// <returns>Model</returns>
+        public string GetModel()
+        {
+            return Model;
+        }
+        /// <summary>
+        /// Set Model
+        /// </summary>
+        /// <param name="Model">Model</param>
+        public void SetModel(string Model)
+        {
+            this.Model = Model;
+        }
+        #endregion
+        #region Get & Set Brand
+        /// <summary>
+        /// Get Brand
+        /// </summary>
+        /// <returns>Brand</returns>
+        public string GetBrand()
+        {
+            return Brand;
+        }
+        /// <summary>
+        /// Set Brand
+        /// </summary>
+        /// <param name="Brand">Brand</param>
+        public void SetBrand(string Brand)
+        {
+            this.Brand = Brand;
+        }
+        #endregion
+        #region Get & Set KLM
+        /// <summary>
+        /// Get KLM
+        /// </summary>
+        /// <returns>KLM</returns>
+        public int GetKLM()
+        {
+            return KLM;
+        }
+        /// <summary>
+        /// Set KLM
+        /// </summary>
+        /// <param name="KLM">KLM</param>
+        public void SetKLM(int KLM)
+        {
+            this.KLM = KLM;
+        }
+        #endregion
+        #region Get & Set OriginalInServiceDate
+        /// <summary>
+        /// Get OriginalInServiceDate
+        /// </summary>
+        /// <returns>OriginalInServiceDate</returns>
+        public DateOnly GetOriginalInServiceDate()
+        {
+            return OriginalInServiceDate;
+        }
+        /// <summary>
+        /// Set OriginalInServiceDate
+        /// </summary>
+        /// <param name="OriginalInServiceDate">OriginalInServiceDate</param>
+        public void SetOriginalInServiceDate(DateOnly OriginalInServiceDate)
+        {
+            this.OriginalInServiceDate = OriginalInServiceDate;
+        }
+        #endregion
+        #region Get & Set Power
+        /// <summary>
+        /// Get Power
+        /// </summary>
+        /// <returns>Power</returns>
+        public int GetPower()
+        {
+            return Power;
+        }
+        /// <summary>
+        /// Set Power
+        /// </summary>
+        /// <param name="Power">Power</param>
+        public void SetPower(int Power)
+        {
+            this.Power = Power;
+        }
         #endregion
         #region Methode
         /// <summary>
         /// method for displaying Car information
         /// </summary>
-
         public void Print()
         {
             Console.WriteLine($"Marque : {Brand}");
@@ -55,7 +161,36 @@ namespace TPVoiture
             Console.WriteLine($"Date de mise en service : {OriginalInServiceDate}");
             Console.WriteLine($"Puissance : {Power}cv");
             Console.WriteLine($"Kilométrage : {KLM} Km");
-        } 
+            Console.Write($"Proprietaire : {Owner}");
+        }
+        /// <summary>
+        /// method for displaying Car information without owner
+        /// </summary>
+        public void PrintWithoutOwner()
+        {
+            Console.WriteLine($"Marque : {Brand}");
+            Console.WriteLine($"Model : {Model}");
+            Console.WriteLine($"Plaque d'immatricualtion : {Registration}");
+            Console.WriteLine($"Date de mise en service : {OriginalInServiceDate}");
+            Console.WriteLine($"Puissance : {Power}cv");
+            Console.WriteLine($"Kilométrage : {KLM} Km");
+        }
+        /// <summary>
+        /// Method to add owner
+        /// </summary>
+        /// <param name="person">Person who own car</param>
+        public void AddOwner(Person person)
+        {
+            this.Owner = person;
+        }
+        /// <summary>
+        /// Method to remove owner
+        /// </summary>
+        /// <param name="person">Remove person form car ownership</param>
+        public void RemoveOwner()
+        {
+            Owner = null;
+        }       
         #endregion
     }
 }
--- TPVoiture/Person.cs
+++ TPVoiture/Person.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.WebSockets;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,35 +13,123 @@ namespace TPVoiture
     /// </summary>
     internal class Person
     {
-        #region Attribute
+        #region attributes
         /// <summary>
         /// Name of the person
         /// </summary>
-        public string Name;
+        private string Name;
         /// <summary>
         /// First name of the person
         /// </summary>
-        public string FirstName;
+        private string FirstName;
         /// <summary>
         /// Age of the person
         /// </summary>
-        public int Age;
+        private int Age;
         /// <summary>
         /// person’s car list
         /// </summary>
-        public List<Car> Cars = new List<Car>();
+        private List<Car> Cars = new List<Car>();
+        #endregion
+        #region Get & Set Name
+        /// <summary>
+        /// Get Name
+        /// </summary>
+        /// <returns>Name</returns>
+        public string GetName()
+        {
+            return Name;
+        }
+        /// <summary>
+        /// Set Name
+        /// </summary>
+        /// <param name="Name">Name</param>
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+        #endregion
+        #region Get & Set FirstName
+        /// <summary>
+        /// Get FirstName
+        /// </summary>
+        /// <returns>FirstName</returns>
+        public string GetFirstName()
+        {
+            return FirstName;
+        }
+        /// <summary>
+        /// Set FirstName
+        /// </summary>
+        /// <param name="FirstName">FirstName</param>
+        public void SetFirstName(string FirstName)
+        {
+            this.FirstName = FirstName;
+        }
+        #endregion
+        #region Get & Set Age
+        /// <summary>
+        /// Get Age
+        /// </summary>
+        /// <returns>Age</returns>
+        public int GetAge()
+        {
+            return Age;
+        }
+        /// <summary>
+        /// Set Age
+        /// </summary>
+        /// <param name="Age">Age</param>
+        public void SetAge(int Age)
+        {
+            this.Age = Age;
+        }
         #endregion
         #region Methode
         /// <summary>
         /// method for displaying Person information
         /// </summary>
-
         public void Print()
         {
             Console.WriteLine($"Nom : {Name}");
             Console.WriteLine($"Prenom : {FirstName}");
             Console.WriteLine($"Age : {Age}");
-        } 
+            Console.WriteLine("Voitures:");
+            Console.WriteLine("");
+            for (int i = 0; i < this.Cars.Count; i++)
+            {
+                this.Cars.ElementAt(i).PrintWithoutOwner();
+                Console.WriteLine("");
+                Console.WriteLine("");
+            }
+            
+        }
+        /// <summary>
+        /// Method to add car
+        /// </summary>
+        /// <param name="Car">Add car to a Person</param>
+        public void AddCar(Car Car)
+        {
+            Cars.Add(Car);
+        }
+        /// <summary>
+        /// Method to remove car
+        /// </summary>
+        /// <param name="car">Remove car from a Person</param>
+        public void RemoveCar(Car car)
+        {
+            Cars.Remove(car);
+        }
+        /// <summary>
+        /// Surcharge methode ToString
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return Name;
+        }
         #endregion
+
+
     }
 }
